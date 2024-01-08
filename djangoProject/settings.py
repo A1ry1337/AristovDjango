6c8a653bc81c2dc4a0466a87c1071a164608a19b
Михаил <mixail-aristov07072003@mail.ru>
2024-01-08 11:18:21 +0500

    добавил шаблон со статикой

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wmj+b7jx+!bs2uokq_phk@^q90p*g3%=s)u0q8%d*ka-u+q^*f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
